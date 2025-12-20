d5cd280948eac19066e19f0f785776f65a0e07a2
ryousiff <Roro2004yousif@gmail.com>
2025-12-20 12:37:25 -0800

    Fix LocalDB connection by using DataDirectory and removing hardcoded paths

--- SCAIS/Program.cs
+++ SCAIS/Program.cs
@@ -1,21 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SCAIS
 {
-    internal static class Program
+    static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Resolve |DataDirectory| to the executable folder so AttachDbFilename=|DataDirectory|\SCAISDB.mdf uses your repo copy at runtime
+            AppDomain.CurrentDomain.SetData("DataDirectory", Application.StartupPath);
+
             Application.Run(new login());
         }
     }
--- SCAIS/Properties/Settings.Designer.cs
+++ SCAIS/Properties/Settings.Designer.cs
@@ -26,8 +26,7 @@ namespace SCAIS.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\ahmed-pc\\Documents\\S" +
-            "CAIS\\SCAIS\\SCAISDB.mdf;Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\SCAISDB.mdf;Integrated Security=True;Connect Timeout=30")]
         public string SCAISDBConnectionString {
             get {
                 return ((string)(this["SCAISDBConnectionString"]));
